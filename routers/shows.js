236e81a44ea68f01dcb997eac2095d5d4ebdadd8
Habib Hassan <habib.hassan@habib.hassan-MacBook-Pro>
2022-12-14 15:44:38 -0600

    routers done

--- routers/shows.js
+++ routers/shows.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { Show } = require("../models/index")
+const { Show, User } = require("../models/index")
 
 router.get("/", async (req, res) =>{
     res.send(await Show.findAll() )
 })
+
 router.get("/:id", async (req, res)=>{
    const show = await Show.findByPk(req.params.id)
     res.send(show)
 })
+
 router.get("/genre/:genre", async (req, res)=>{
     const show = await Show.findAll({
         where:{
@@ -18,13 +20,26 @@ router.get("/genre/:genre", async (req, res)=>{
      res.send(show)
  })
 
-// router.put("/:id", async (req, res) =>{
-//     const newShowInfo = req.body;
-//     const ShowToUpdate = await Show.findByPk(req.params.id);
-//     await ShowToUpdate.update(newShowInfo)
-//     res.send(await Show.findAll())
-// })
+router.put("/rating/:id/", async (req, res) =>{
+    const show = await Show.findByPk(req.params.id);
+    await show.update({rating:req.body})
+    res.send(await Show.findAll())
+})
 
+router.put("/status/:id/", async (req, res) =>{
+    const show = await Show.findByPk(req.params.id);
+    await show.update({status:req.body})
+    res.send(await Show.findAll())
+})
+
+router.delete("/:id", async(req,res)=>{
+  const deletedShow=  Show.destroy({
+        where:{
+            id : req.params.id
+        }
+    })
+    res.send(await Show.findAll())
+})
 
 
 module.exports = router;
--- routers/users.js
+++ routers/users.js
@@ -9,24 +9,16 @@ router.get("/:id", async (req,res) =>{
     res.send(await User.findByPk(req.params.id))
 })
 router.get("/userSeen/:id", async (req, res) =>{
-    const userHasSeen = await Show.findAll({
-        where:{
-            userId: req.params.id
-        }
+    const user = await User.findByPk(req.params.id,{
+        include: Show
     })
-    res.send(userHasSeen)
+    res.send(user.shows)
 })
-router.put("/updated/:id/", async (req, res) =>{
-    const showInfo = req.body;
-    
-    const showToUpdate = await Show.findAll({
-        where:{
-            userId: req.params.id
-        }
-    })
-    
-    await showToUpdate.update(showInfo)
-    res.send(await Show.findAll())
+router.put("/user/:id/show/:id2", async (req, res) =>{
+  const foundUser = await User.findByPk(req.params.id);
+  const show = await Show.findByPk(req.params.id2)
+  await foundUser.addShow(show) 
+  res.send(await Show.findAll())
 })
 
 module.exports = router;
